b30f0e516eb6abd0e6d2a9c00127a536b20cbd96
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2019-05-27 15:57:35 -0400

    Update usuario.php

--- class/usuario.php
+++ class/usuario.php
@@ -15,10 +15,10 @@ class usuario extends bd{
 		//print_r($valores);
 		return $this->updateRow($valores,"CodUsuario=$CodUsuario");
 	}
-	
+
 	function loginUsuarios($Usuario,$Password){
-		$this->campos=array("count(*) as Can,CodUsuario,Nivel");	
-		return $this->getRecords("Usuario='$Usuario' and Pass2=MD5('$Password') and Activo=1");
+		$this->campos=array("count(*) as Can,CodUsuario,Nivel");
+		return $this->getRecords("Usuario='$Usuario' and Pass=MD5('$Password') and Activo=1");
 	}
 }
 ?>
