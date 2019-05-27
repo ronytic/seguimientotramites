7ca2cc9571880fe7f2c1211760b417ce7d992f4f
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2019-05-27 15:06:25 -0400

    Create basededatos.php

---
+++ basededatos.php
@@ -0,0 +1,16 @@
+<?php
+$host="localhost";
+$user="root";
+$pass="";
+$database="seguimientotramites";
+
+/*$user="redcampu_csb";
+$pass="83034888";
+$database="redcampu_csb1";*/
+
+/*Configuración de Idioma del Sistema*/
+date_default_timezone_set('America/La_Paz');
+setlocale(LC_CTYPE, "es_ES");
+setlocale(LC_ALL, 'sp');
+setlocale(LC_ALL,"es_ES@euro","es_ES","esp");
+?>
