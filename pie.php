b5289c593ea03c9bf558fe71083e253463ab72cb
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2019-05-27 15:07:06 -0400

    Create pie.php

---
+++ pie.php
@@ -0,0 +1,41 @@
+               </div>
+            </div>
+        </div>
+    </div>    
+   <!----------------->
+   </div>
+</div>
+
+    
+
+    <!-- Footer-->
+    <footer class="footer">
+        <span class="pull-right" style="color: #ffffff;">
+            Sistema Desarrollado por: <a href="http://fb.com/ronaldnina" target="_blank" style="color: #ffffff;">Ronald Nina</a>
+        </span>
+        Sistema de <?php echo $tituloSistema;?>
+    </footer>
+
+</div>
+
+<!-- Vendor scripts -->
+
+<script src="<?php echo $folder?>js/core/jquery-ui.min.js"></script>
+<script src="<?php echo $folder?>js/core/slimScroll/jquery.slimscroll.min.js"></script>
+<script src="<?php echo $folder?>js/core/bootstrap.min.js"></script>
+<script src="<?php echo $folder?>js/core/metismenu/metisMenu.min.js"></script>
+<script src="<?php echo $folder?>js/core/icheck.min.js"></script>
+<script src="<?php echo $folder?>js/core/peity/jquery.peity.min.js"></script>
+<script src="<?php echo $folder?>js/core/sparkline/index.js"></script>
+
+<!-- App scripts -->
+<script src="<?php echo $folder?>js/core/homer.js"></script>
+<script src="<?php echo $folder?>js/core/charts.js"></script>
+<script src="<?php echo $folder?>js/core/jquery.form.js"></script>
+<script src="<?php echo $folder?>js/core/cargadortotal.js"></script>
+
+    <script>
+    </script>
+
+</body>
+</html>
