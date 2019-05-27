d683d30bb99257a2a3a4e6197a0a4d8aae891b60
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2019-05-27 15:07:09 -0400

    Create configuracion.php

---
+++ configuracion.php
@@ -0,0 +1,14 @@
+<?php
+$directory="seguimientotramites/";		//Sub Directorio, Ej: "sistemacolegio/"
+							//si su sistema e encuentre directamente en el dominio sin ningun subdirectorio dejelo vacio Ej: ""
+$Internet=0; //¿Donde se Encuentra su Sistema?
+			// 0 en Sistema Local
+			// 1 en Internet
+			// 3 Solamente en LOCAL no Internet(Red de Area Local)
+
+$tituloSistema="Seguimiento Tramites";
+$lemaSistema="";
+$ta4="0aXzh++RAPaeH9XPUXzAaLjYAhOByL5jWvmip5hP0CSy2pP+8wyPegfbzLZu13exmtof0B3aEtM9Wid05YtJmQ==";
+$tp="D3W1N8E0E4B4B4";
+date_default_timezone_set('America/La_Paz');
+?>
