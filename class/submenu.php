06ede6471298c197c0cfd149d32c4da5b18db38e
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2019-06-01 01:15:21 -0400

    Update submenu.php

--- class/submenu.php
+++ class/submenu.php
@@ -6,19 +6,17 @@ class submenu extends bd{
 		$this->campos=array('Nombre','Url');
 		switch($Nivel){
 			case "1":{return $this->getRecords(" Admin=1 and CodMenu=$Menu and Activo=1","Orden");}break;
-			case "2":{return $this->getRecords(" Gerente=1 and CodMenu=$Menu and Activo=1","Orden");}break;
-			case "3":{return $this->getRecords(" Administrador=1 and CodMenu=$Menu and Activo=1","Orden");}break;
-			case "4":{return $this->getRecords(" Vendedor=1 and CodMenu=$Menu and Activo=1","Orden");}break;
-			case "5":{return $this->getRecords(" Almacen=1 and CodMenu=$Menu and Activo=1","Orden");}break;
-	}
+			case "2":{return $this->getRecords(" Mae=1 and CodMenu=$Menu and Activo=1","Orden");}break;
+			case "3":{return $this->getRecords(" Subdireccion=1 and CodMenu=$Menu and Activo=1","Orden");}break;
+			case "4":{return $this->getRecords(" Recepcion=1 and CodMenu=$Menu and Activo=1","Orden");}break;
+		}
 	}
 	function verificar($Directorio,$Nivel){
 		switch($Nivel){
 			case "1":{return $this->getRecords("Url='$Directorio' and Admin=1 and Activo=1","Orden");}break;
-			case "2":{return $this->getRecords("Url='$Directorio' and  Gerente=1 and Activo=1","Orden");}break;
-			case "3":{return $this->getRecords("Url='$Directorio' and  Administrador=1 and Activo=1","Orden");}break;
-			case "4":{return $this->getRecords("Url='$Directorio' and  Vendedor=1 and Activo=1","Orden");}break;
-			case "5":{return $this->getRecords("Url='$Directorio' and  Almacen=1 and Activo=1","Orden");}break;
+			case "2":{return $this->getRecords("Url='$Directorio' and  Mae=1 and Activo=1","Orden");}break;
+			case "3":{return $this->getRecords("Url='$Directorio' and  Subdireccion=1 and Activo=1","Orden");}break;
+			case "4":{return $this->getRecords("Url='$Directorio' and  Recepcion=1 and Activo=1","Orden");}break;
 		}
 	}
 }
