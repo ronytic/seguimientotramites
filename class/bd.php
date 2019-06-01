bba5af70ae7a3421e2a2d74227f92a1736603096
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2019-06-01 01:15:11 -0400

    Update bd.php

--- class/bd.php
+++ class/bd.php
@@ -89,7 +89,7 @@ class bd{
 			$nombretabla=mb_strtolower($this->tabla,"utf8");
 		}
 		$query ="SELECT $camposs FROM {$nombretabla} $where $group $order $count";
-		//echo $query."<br>";
+		// echo $query."<br>";
 		return $this->sql ($query);
 	}
 	public function getRecord ($id)
