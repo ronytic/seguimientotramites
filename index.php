453a7df0f4466a12206807919989367f5ecf4875
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2019-05-31 23:26:53 -0400

    Update index.php

--- index.php
+++ index.php
@@ -7,79 +7,56 @@ $Lema="";
 ?>
 <?php include_once("cabecerahtml.php");?>
 <?php include_once("cabecera.php");?>
-                <div class="col-lg-10 col-lg-offset-1">
-                    <?php
-                      /*
-                    ?>
-                    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-                      <!-- Indicators -->
-                      <ol class="carousel-indicators">
-                        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-                        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-                        <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-                        <li data-target="#carousel-example-generic" data-slide-to="3"></li>
-                      </ol>
-
-                      <!-- Wrapper for slides -->
-                      <div class="carousel-inner" role="listbox">
-                        <div class="item active">
-                            <center>
-                          <img src="imagenes/inicio/2.png" alt="">
-                          </center>
-                          <div class="carousel-caption">
-                            ...
-                          </div>
-                        </div>
-                        <div class="item">
-                            <center>
-                          <img src="imagenes/inicio/1.jpg" alt="">
-                            </center>
-                          <div class="carousel-caption">
-                            ...
-                          </div>
-                        </div>
-                        <div class="item">
-                            <center>
-                          <img src="imagenes/inicio/3.jpg" alt="">
-                            </center>
-                          <div class="carousel-caption">
-                            ...
-                          </div>
-                        </div>
-                        <div class="item">
-                            <center>
-                          <img src="imagenes/inicio/4.jpg" alt="">
-                            </center>
-                          <div class="carousel-caption">
-                            ...
-                          </div>
-                        </div>
-
-                      </div>
-
-                      <!-- Controls -->
-                      <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-                        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                        <span class="sr-only">Anterior</span>
-                      </a>
-                      <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-                        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                        <span class="sr-only">Siguiente</span>
-                      </a>
-                    </div>
-
-                  <?php
-                    */
-                  ?>
-                </div>
-            </div>
+<div class="col-lg-12 text-center">
+  <div class="row">
+    <div class="col-lg-12">
+      <h3>Bienvenido al Sistema</h3>
+      <hr>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-lg-6">
+        <div class="panel panel-warning">
+          <div class="panel-heading ">
+            <h5>
+              Visión
+            </h5>
+          </div>
+          <div class="panel-body text-justify">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea dolores unde veritatis, aliquid debitis voluptatibus, odio totam incidunt iusto, tenetur nesciunt quia tempora suscipit amet? Consequatur dolore quos veritatis provident nulla, voluptates, aperiam nam vero at placeat nemo doloremque dolores animi, veniam delectus perferendis reprehenderit? Officiis, sapiente dolor! Fuga, eius!
+          </div>
         </div>
-        <div class="panel-footer">
-        <span class="pull-right">
-
-        </span>
-
+        <div class="panel panel-warning">
+          <div class="panel-heading ">
+            <h5>
+              Misión
+            </h5>
+          </div>
+          <div class="panel-body text-justify">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea dolores unde veritatis, aliquid debitis voluptatibus, odio totam incidunt iusto, tenetur nesciunt quia tempora suscipit amet? Consequatur dolore quos veritatis provident nulla, voluptates, aperiam nam vero at placeat nemo doloremque dolores animi, veniam delectus perferendis reprehenderit? Officiis, sapiente dolor! Fuga, eius!
+          </div>
         </div>
-
-
+    </div>
+    <div class="col-lg-6">
+    <div class="panel panel-warning">
+          <div class="panel-heading ">
+            <h5>
+              Instrucciones
+            </h5>
+          </div>
+          <div class="panel-body text-justify">
+              <ul>
+                <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo quae, maiores non provident labore et praesentium molestias! Reiciendis illo at aspernatur nam rerum laudantium nostrum!</li>
+                <li>Aperiam ipsam adipisci accusantium quibusdam cupiditate magnam error voluptatum sit, tempora reiciendis. Vel mollitia deserunt deleniti, est qui explicabo quas. Natus molestias eum esse ratione.</li>
+                <li>Perferendis sunt quis nisi culpa. Tempora asperiores quasi eligendi molestias voluptate, obcaecati aperiam minima, ducimus deserunt explicabo dolorem voluptas delectus facere in labore architecto repudiandae?</li>
+                <li>Consectetur atque quidem aut nulla rerum ad, exercitationem beatae aliquam dolor enim laboriosam facilis commodi! Quia deleniti perferendis similique nisi beatae repellendus ipsa rem praesentium!</li>
+                <li>Ex possimus voluptates soluta natus corrupti voluptatibus perferendis nostrum? Placeat ratione quod libero voluptate, dolores iste temporibus tempore sapiente quidem distinctio dolor eligendi iure fugiat!</li>
+                <li>Tenetur corporis quasi neque repellendus nulla sed culpa esse, sit, similique vero quo explicabo reiciendis saepe minus voluptatibus, ratione voluptate illum eius vitae nihil tempore.</li>
+                <li>Quidem id aliquam soluta reiciendis possimus, illo similique itaque illum quaerat dicta minus odio ipsam delectus voluptate, cum esse fugiat amet impedit labore atque obcaecati.</li>
+              </ul>
+          </div>
+        </div>
+    </div>
+  </div>
+</div>
 <?php include_once("pie.php");?>
